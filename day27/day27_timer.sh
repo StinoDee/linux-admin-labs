35a9e040948b036ac4abce5ffa3794e9110b9493
Stino Dee <stinodee@gmail.com>
2026-02-26 23:05:31 +0200

    Add Day 27 Hands-On Lab

---
+++ day27/day27_timer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Day 27: Timer log script
+mkdir -p ~/linux_lab/day27
+echo "Hello from Day 27 Timer! $(date)" >> ~/linux_lab/day27/timer.log
---
+++ day27/hello_timer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Hello from Day 27 Timer!" >> ~/linux_lab/day27/timer.log
